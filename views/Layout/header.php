9454f4950004a2cd3b566451319f954c7797b168
413xsalis <alexsanchez.az2005@gmail.com>
2025-05-13 08:12:03 -0500

    Implementacion del Layout

---
+++ views/Layout/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <header>
+
+            <div class="logo-ZM" >
+                <img class="Logo" src="../assets/img/LogoZM.png" alt="Imagen Logo">
+            </div>
+
+            <div class="Titulo">
+                <img class="LogoT" src="../assets/img/tituloCentro.png" alt="Titulo/Zm">
+            </div>
+
+            <div class="menu">
+                <img class="IconoMenu" src="../assets/img/menu.png" alt="Icono/Menu">
+            </div>
+
+    </header>
+
+</body>
+</html>
