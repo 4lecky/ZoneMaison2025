6bfb826a09463507be20dd268d9884bc05bc17ce
DSnaiders <mazorcodilan@gmail.com>
2025-05-23 11:43:56 -0500

    Adriana Rodriguez, actualizacion views y css de parquedero

--- views/parqueadero.php
+++ views/parqueadero.php
@@ -1,39 +1,42 @@
+
+<?php 
+require_once "./layout/header.php"
+?>
+
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>ZoneMaisons - Registro Vehículos/Cobro de tarifas</title>
-  <link rel="stylesheet" href="../../assets/Css/global.css" />
+  <link rel="stylesheet" href="../../assets/Css/globals.css" />
   <link rel="stylesheet" href="../assets/Css/parqueadero.css" />
 
 </head>
 <body>
 
 
-<?php 
-require_once "./layout/header.php"
-?>
-
   <aside class="sidebar">
-
-    <!-- Nueva sección de solo imágenes -->
-    <div class="iconos-botones">
-
-      <img src="../assets/img/RegistroVehiculo.png" alt="Icono Registro Vehículo" />
-      <div class="texto-hover">AQUI LLENA LA INFORMACION DE LO QUE SIGNIFICA EL BOTON</div>
-      <img src="../assets/img/cobroTarifa.png" alt="Icono Cobro Tarifas" />
-      <div class="texto_hover">AQUI LLENA LA INFORMACION DE LO QUE SIGNIFICA EL BOTON</div>
+    <div class="contenedor-botones">
       
+      <!-- Primer bloque: Registro Vehículo -->
+      <div class="bloque">
+        <img src="../assets/img/RegistroVehiculo.png" alt="Icono Registro Vehículo" class="img_parqueadero" />
+        <p class="texto-hover-parqueadero">Registra fácilmente los vehículos y sus propietarios. Llena el formulario, verifica los datos y guarda la información. ¡Ahorra tiempo y mantén un control seguro para toda la comunidad!</p>
+        <button class="menu-button" onclick="mostrarFormulario('formularioRegistro')">REGISTRO DE VEHÍCULOS</button>
+      </div>
+
+      <!-- Segundo bloque: Cobro Tarifas -->
+      <div class="bloque">
+        <img src="../assets/img/cobroTarifa.png" alt="Icono Cobro Tarifas" class="img_parqueadero" />
+        <p class="texto-hover-parqueadero">Registra y calcula fácilmente el cobro por uso de parqueadero. Ingresa los datos necesarios y genera el valor correspondiente. ¡Agiliza el proceso y mantén un control claro y transparente para todos!</p>
+        <button class="menu-button" onclick="mostrarFormulario('formularioCobro')">COBRO TARIFAS</button>
+      </div>
+
     </div>
-
-    <div class="iconos-botones">
-    <button class="menu-button" onclick="mostrarFormulario('formularioRegistro')">REGISTRO DE VEHICULOS</button>
-    <button class="menu-button" onclick="mostrarFormulario('formularioCobro')">COBRO TARIFAS</button>
-</div>
-
-
   </aside>
+
 
 
     <!-- Formulario Registro de Vehiculos-->
@@ -222,9 +225,9 @@ require_once "./layout/header.php"
       </main>
 
 
-      <?php
-        require_once "./Layout/footer.php"
-      ?>
+<?php 
+require_once "./Layout/footer.php"
+?>
 
 
 <script src="../assets/Js/parqueadero.js"></script>
