2f73a8f00e68e7957bd6b8c29e1ec33ef33fa761
4lecky <lunitaromero1221@gmail.com>
2025-05-30 11:25:47 -0500

    Ver contraseña añadido al login (Luna Salas)

--- assets/Js/login.js
+++ assets/Js/login.js
@@ -1,15 +1,52 @@
-document.getElementById('formulario_ingreso').addEventListener('submit', function (e) {
+// document.getElementById('formulario_ingreso').addEventListener('submit', function (e) {
 
-    const email = this.Email.value;
-    const password = this.Password.value;
+//     const email = this.Email.value;
+//     const password = this.Password.value;
 
-    if (!email.includes('@')) {
-        alert("Correo electrónico no válido.");
-        e.preventDefault();
+//     if (!email.includes('@')) {
+//         alert("Correo electrónico no válido.");
+//         e.preventDefault();
+//     }
+
+//     if (password.trim().length < 6) {
+//         alert("La contraseña debe tener al menos 8 caracteres.");
+//         e.preventDefault();
+//     }
+
+// });
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('formulario_ingreso');
+    if (form) {
+        form.addEventListener('submit', function (e) {
+            const email = this.Email.value;
+            const password = this.Password.value;
+    
+            if (!email.includes('@')) {
+                alert("Correo electrónico no válido.");
+                e.preventDefault();
+            }
+    
+            if (password.trim().length < 8) {
+                alert("La contraseña debe tener al menos 8 caracteres.");
+                e.preventDefault();
+            }
+        });
     }
-
-    if (password.trim().length < 6) {
-        alert("La contraseña debe tener al menos 8 caracteres.");
-        e.preventDefault();
+    
+    const togglePassword = document.getElementById('toggle-password');
+    if (togglePassword) {
+        togglePassword.addEventListener('click', function() {
+            const passwordInput = document.getElementById('password-input');
+            const eyeIcon = document.getElementById('eye-icon');
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                eyeIcon.classList.remove('fa-eye');
+                eyeIcon.classList.add('fa-eye-slash');
+            } else {
+                passwordInput.type = 'password';
+                eyeIcon.classList.remove('fa-eye-slash');
+                eyeIcon.classList.add('fa-eye');
+            }
+        });
     }
 });
