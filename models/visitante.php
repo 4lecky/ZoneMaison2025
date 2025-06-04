519872c20b6e1150205b699ec53d2e1d33b0bc05
AndresGutierrez2908 <est3700@gmail.com>
2025-06-04 07:29:25 -0500

    Modifique estilo Formulario(Andres)

--- models/visitante.php
+++ models/visitante.php
@@ -1,10 +1,9 @@
 <?php
-<<<<<<< HEAD
 
 require_once __DIR__."/../config/db.php";
-=======
+
 require_once __DIR__ . '/../config/db.php';
->>>>>>> 2f73a8f00e68e7957bd6b8c29e1ec33ef33fa761
+
 class Visitante {
     private $pdo;
 
