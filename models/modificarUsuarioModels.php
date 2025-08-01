9383b2c054a85a6209e2aa3ab5273c0f3f3ab6bc
4lecky <lunitaromero1221@gmail.com>
2025-08-01 11:50:41 -0500

    Modificación form crud

--- models/modificarUsuarioModels.php
+++ models/modificarUsuarioModels.php
@@ -40,76 +40,99 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modificar Usuario</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/Css/crud/formCrud.css">
+    <link rel="stylesheet" href="../assets/Css/globals.css" />
 
 </head>
 
-<body>
+<body class='body_crud'>
 
 
     <form method="POST" action="../controller/ModificarUsuarioController.php" class="container_form_crud">
-        <h3> Modificar usuario </h3>
+        <h3> Aqui puede modificar su usuario</h3>
+        <fieldset class='fieldset_crud'>
+            <legend class='legend_crud' > Modificar usuario </legend>
+            <br>
+            <!-- Con esto guardamos el id del usuario y es enviado al controlador -->
+            <input type="hidden" name="cc" value="<?= htmlspecialchars($_GET['cc']) ?>">
 
-        <!-- Con esto guardamos el id del usuario y es enviado al controlador -->
-        <input type="hidden" name="cc" value="<?= htmlspecialchars($_GET['cc']) ?>">
+            <div class="columnaCrud">
 
-        <div class="col-md-6">
-            <label for="validationCustom01" class="form-label">Correo Electronico</label>
-            <input type="text" class="form-control" id="validationCustom01" name="correo" value="<?= $datos->usu_correo ?>" required>
-
-        </div>
-
-        <div class="col-md-6">
-            <label for="validationCustom02" class="form-label">Torre de residencia</label>
-            <input type="text" class="form-control" id="validationCustom02" name="torre" value="<?= $datos->usu_torre_residencia ?>" required>
-
-        </div>
-
-        <div class="col-md-6">
-            <label for="validationCustomUsername" class="form-label">Apartamento de residencia</label>
-            <div class="input-group has-validation">
-                <input type="text" class="form-control" id="validationCustomUsername" name="apartamento" value="<?= $datos->usu_apartamento_residencia ?>" required>
-
-            </div>
-
-            <div class="container_selects_crud">
-
-                <div class="col-md-5">
-
-                    <label for="validationCustom04" class="form-label">Estado</label>
-                    <select class="form-select" id="validationCustom04" name="estado">
-
-                        <option value="<?= $datos->usu_estado ?>">Elija una opción</option>
-                        <option> Activo </option>
-
-                    </select>
+                <div class="inputs_crud_modificar">
+                    <label for="validationCustom01" class="form-label">Correo Electronico</label>
+                    <input type="text" class="form-control" id="validationCustom01" name="correo" value="<?= $datos->usu_correo ?>" required>
 
                 </div>
 
-
-                <div class="col-md-5">
-
-                    <label for="validationCustom04" class="form-label">Rol</label>
-                    <select class="form-select" id="validationCustom04" name="rol">
-
-                        <option value="<?= $datos->usu_rol ?>">Elija una opción</option>
-                        <option> Administrador </option>
-                        <option> Residente </option>
-                        <option> Propietario </option>
-                        <option> Vigilante </option>
-
-                    </select>
+                <div class="inputs_crud_modificar">
+                    <label for="validationCustom02" class="form-label">Torre de residencia</label>
+                    <input type="text" class="form-control" id="validationCustom02" name="torre" value="<?= $datos->usu_torre_residencia ?>" required>
 
                 </div>
 
             </div>
 
-            <div class="container-btn-crud">
-                <button type="submit" class="btn btn-form-crud btn-primary" name="btn-confirmar" value="ok"> Confirmar </button>
-                <button type="button" class="btn btn-form-crud btn-primary" onclick="window.location.href='../views/crud.php';"> Cancelar </button>
+            <div class="columnaCrud">
+
+                <div class="inputs_crud_modificar">
+                    <label for="validationCustomUsername" class="form-label">Apartamento de residencia</label>
+                    <div class="input-group has-validation">
+                        <input type="text" class="form-control" id="validationCustomUsername" name="apartamento" value="<?= $datos->usu_apartamento_residencia ?>" required>
+                    </div>
+                </div>
+
+                <div class="select_crud_modificar">
+
+                        <label for="validationCustom04" class="form-label">Estado</label>
+                        <select class="form-select" id="validationCustom04" name="estado">
+
+                            <option value="<?= $datos->usu_estado ?>">Elija una opción</option>
+                            <option> Activo </option>
+                            <option> Inactivo </option>
+
+                        </select>
+
+                 </div>
 
             </div>
+
+            <div class="columnaCrud">
+
+                    <div class="select_crud_modificar">
+
+                        <label for="validationCustom04" class="form-label">Rol</label>
+                        <select class="form-select" id="validationCustom04" name="rol">
+
+                            <option value="<?= $datos->usu_rol ?>">Elija una opción</option>
+                            <option> Administrador </option>
+                            <option> Residente </option>
+                            <option> Propietario </option>
+                            <option> Vigilante </option>
+
+                        </select>
+
+                    </div>
+
+                    <div class="select_crud_modificar">
+
+                        <label for="validationCustom04" class="form-label">Pago de la administración</label>
+                        <select class="form-select" id="validationCustom04" name="mora">
+
+                            <option value="">Elija una opción</option>
+                            <option value="1"> Pagado </option>
+                            <option value="2"> Pendiente </option>
+                        </select>
+                    </div>
+
+            </div>
+        </fieldset>
+
+                <div class="container-btn-crud">
+                    <button type="submit" class="btn btn-form-crud " name="btn-confirmar" value="ok"> Confirmar </button>
+                    <button type="button" class="btn btn-form-crud " onclick="window.location.href='../views/crud.php';"> Cancelar </button>
+
+                </div>
+
 
     </form>
 
