35255590c48f2a4961b67aa7673f6ad06f7799cc
4lecky <lunitaromero1221@gmail.com>
2025-09-06 15:10:21 -0500

    Ajustes 3

--- assets/Js/updatePassword.js
+++ assets/Js/updatePassword.js
@@ -1,23 +1,23 @@
-window.addEventListener("DOMContentLoaded",()  => {
+    window.addEventListener("DOMContentLoaded",()  => {
 
-    document.getElementById('form_update_password').addEventListener('submit', function (e) {
-        const inputs = this.querySelectorAll('input');
-        for (let input of inputs) {
-            if (!input.value.trim()) {
-                alert(`Por favor completa el campo: ${input.name}`);
-                input.focus();
-                e.preventDefault();
-                return false;
+        document.getElementById('form_update_password').addEventListener('submit', function (e) {
+            const inputs = this.querySelectorAll('input');
+            for (let input of inputs) {
+                if (!input.value.trim()) {
+                    alert(`Por favor completa el campo: ${input.name}`);
+                    input.focus();
+                    e.preventDefault();
+                    return false;
+                }
             }
-        }
-    
-        const password = this.password.value;
-        let expresionRegularPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,15}$/gm;
-    
-        if (expresionRegularPassword.test(password)==false) {
-            alert("La contraseña debe tener al entre 8 y 10 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. Puede tener otros símbolos");
-            e.preventDefault();
-        }
+        
+            const password = this.password.value;
+            let expresionRegularPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,15}$/gm;
+        
+            if (expresionRegularPassword.test(password)==false) {
+                alert("La contraseña debe tener al entre 8 y 15 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. Puede tener otros símbolos");
+                e.preventDefault();
+            }
+        });
     });
-});
-    
+        
--- controller/AuthController.php
+++ controller/AuthController.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../models/authModel.php';
+require_once __DIR__ . '/../models/AuthModel.php';
 // require_once __DIR__ . '/../config/db.php';
 
 class AuthController {
